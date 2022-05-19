f4c95793542943cce47580d21561094b0f79578f
Josh West <bigdaddydas@gmail.com>
2022-05-18 19:09:29 -0600

    CSS styling

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/deep-thoughts',
   {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
